006bf7720cb3926c8f9f2ad4cec385274ac002db
Ishan Dutta <ishandutta0098@gmail.com>
2020-11-06 23:44:48 +0530

    Update sort.cpp

--- C++ STL/sort.cpp
+++ C++ STL/sort.cpp
@@ -1,1 +1,11 @@
+//Declare Array
+int arr = {...}
 
+//Length of array
+int n = sizeof(arr) / sizeof(arr[0]); 
+
+//Ascending
+sort(arr, arr + n);
+
+//Descending
+sort(arr, arr + n, greater<int>());
