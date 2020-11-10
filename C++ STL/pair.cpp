38fd6d2ad77fce395732d9aa8715030294960d3b
Ishan Dutta <ishandutta0098@gmail.com>
2020-11-10 17:43:19 +0530

    Pairs

---
+++ C++ STL/pair.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+	
+	void ExplainPair() 
+	{
+	    
+	    //pair<int, int> p1 = {1,3};
+	    
+	    //cout<<p1.first<<" "<<p1.second<<endl;
+	    //prints 1 2 
+	    
+	    //PAIR OF PAIR:
+	    //pair<int, pair<int, int>> p2 = {1, {3,4}};
+	    
+	    //cout<<p2.first<<" "<<p2.second.second<<" "<<p2.second.first<<endl;
+	    //prints 1 4 3 
+	    
+	    //PAIR OF ARRAY:
+	    pair<int, int> arr[] = {{1,2}, {3,4}, {5,6}};
+	    
+	    cout<<arr[1].second;
+	    //prints 2;
+	}
+	
+	int main() {
+	ExplainPair();
+	return 0;
+}
