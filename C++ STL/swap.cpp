328484ed3d2497a7206d5d1ed3698007a0222f5f
Ishan Dutta <ishandutta0098@gmail.com>
2020-11-12 19:26:52 +0530

    Swap 2 elements

---
+++ C++ STL/swap.cpp
@@ -0,0 +1,16 @@
+//Swap 2 numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 10, b=20;
+    
+    cout<<a<<" "<<b<<endl; //prints 10 20
+    
+    swap(a,b);
+    
+    cout<<a<<" "<<b; //prints 20 10
+    return 0;
+}
