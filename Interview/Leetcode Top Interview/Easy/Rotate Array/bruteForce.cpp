9f343c81ba4b0b26e8c0e7007645cab65e629e70
Ishan Dutta <ishandutta0098@gmail.com>
2020-11-15 16:20:05 +0530

    Brute Force | 2 Vectors - Extra Space

---
+++ Interview/Leetcode Top Interview/Easy/Rotate Array/bruteForce.cpp
@@ -0,0 +1,24 @@
+//ROTATE ARRAY
+
+//Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+    
+      vector<int> v1; //Second vector to store the required array.
+      
+      for(int i = nums.size() - k; i < nums.size(); i++) 
+       v1.push_back(v[i]);
+    
+      for(int i = 0; i < k+1; i++)
+       v1.push_back(v[i]);
+    }
+};
+
+
+
+ 
